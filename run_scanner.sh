3deec299775304af98668b3cd06eb0c7af335d34
Houston <qambarov2812@gmail.com>
2023-06-04 15:24:56 -0700

    UI improvements

--- run_scanner.sh
+++ run_scanner.sh
@@ -9,8 +9,6 @@ echo "Authentication key: $auth_key"
 touch scanner/key.env 
 echo "KEY=$auth_key" > scanner/key.env 
 
-# docker run -d --network host redislabs/redismod
-
-#sleep(10) 
+docker run -d --name redis-stack-server -p 6379:6379 redis/redis-stack-server:latest
 
 docker-compose -f ./scanner/docker-compose.yml up 
